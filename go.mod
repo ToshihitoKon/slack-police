7fc56e448305c1a5563835577d81e1592ad6a17c
Toshihito Kon <toshihito.kon@gmail.com>
2021-02-24 23:01:26 +0900

    socket mode

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.15
 
 require (
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/slack-go/slack v0.8.0 // indirect
+	github.com/slack-go/slack v0.8.1
+	golang.org/x/tools v0.1.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,98 +1,114 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
-	"strconv"
 
 	"github.com/slack-go/slack"
 	"github.com/slack-go/slack/slackevents"
+	"github.com/slack-go/slack/socketmode"
 )
 
-var (
-	token           = os.Getenv("SLAKC_POLICE_TOKEN")
-	signingSecret   = os.Getenv("SLACK_POLICE_SIGNING_SECRET")
-	policeChannelID = os.Getenv("SLACK_POLICE_EMOJI_CHANNNEL_ID")
-	port            = 5002
-	api             = slack.New(token)
-)
+func mustEnv(name string) (string, bool) {
+	value := os.Getenv(name)
+	if name == "" {
+		log.Println("must ", name)
+		return "", false
+	}
+	return value, true
+}
 
 func main() {
+	isValid := true
 
-	http.HandleFunc("/events-endpoint", func(w http.ResponseWriter, r *http.Request) {
-		body, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		sv, err := slack.NewSecretsVerifier(r.Header, signingSecret)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			return
-		}
-		if _, err := sv.Write(body); err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
-		if err := sv.Ensure(); err != nil {
-			w.WriteHeader(http.StatusUnauthorized)
-			return
-		}
-		eventsAPIEvent, err := slackevents.ParseEvent(json.RawMessage(body), slackevents.OptionNoVerifyToken())
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			return
-		}
+	botToken, ok := mustEnv("SLACK_POLICE_BOT_TOKEN")
+	if !ok {
+		isValid = false
+	}
+	appToken, ok := mustEnv("SLACK_POLICE_APP_TOKEN")
+	if !ok {
+		isValid = false
+	}
+	policeChannelID, ok := mustEnv("SLACK_POLICE_EMOJI_CHANNEL_ID")
+	if !ok {
+		isValid = false
+	}
+	if !isValid {
+		return
+	}
 
-		if eventsAPIEvent.Type == slackevents.URLVerification {
-			var r *slackevents.ChallengeResponse
-			err := json.Unmarshal([]byte(body), &r)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-			w.Header().Set("Content-Type", "text")
-			w.Write([]byte(r.Challenge))
-		}
-		if eventsAPIEvent.Type == slackevents.CallbackEvent {
-			innerEvent := eventsAPIEvent.InnerEvent
-			log.Println("recieved: ", innerEvent.Type)
+	// clients
+	api := slack.New(
+		botToken,
+		//slack.OptionDebug(true),
+		//slack.OptionLog(log.New(os.Stdout, "api: ", log.Lshortfile|log.LstdFlags)),
+		slack.OptionAppLevelToken(appToken),
+	)
+	client := socketmode.New(
+		api,
+		//socketmode.OptionDebug(true),
+		//socketmode.OptionLog(log.New(os.Stdout, "socketmode: ", log.Lshortfile|log.LstdFlags)),
+	)
+	go func(api *slack.Client, policeChannelID string) {
+		for evt := range client.Events {
+			switch evt.Type {
+			case socketmode.EventTypeConnecting:
+				fmt.Println("Connecting to Slack with Socket Mode...")
+			case socketmode.EventTypeConnectionError:
+				fmt.Println("Connection failed. Retrying later...")
+			case socketmode.EventTypeConnected:
+				fmt.Println("Connected to Slack with Socket Mode.")
+			case socketmode.EventTypeEventsAPI:
+				eventsAPIEvent, ok := evt.Data.(slackevents.EventsAPIEvent)
+				if !ok {
+					fmt.Printf("Ignored %+v\n", evt)
+					continue
+				}
+				client.Ack(*evt.Request)
 
-			switch ev := innerEvent.Data.(type) {
-			case *slackevents.EmojiChangedEvent:
-				switch ev.Subtype {
-				case "add":
-					log.Println(
-						api.PostMessage(
-							policeChannelID,
-							slack.MsgOptionText(
-								fmt.Sprintf("絵文字警察です👮\n:%s: %s が追加されました", ev.Name, ev.Name),
-								false)))
-				case "remove":
-					log.Println(
-						api.PostMessage(
-							policeChannelID,
-							slack.MsgOptionText(
-								fmt.Sprintf("絵文字警察です👮\n%s が消えました", ev.Names[0]),
-								false)))
-				case "rename":
-					log.Println(
-						api.PostMessage(
-							policeChannelID,
-							slack.MsgOptionText(
-								fmt.Sprintf("絵文字警察です👮\n:%s: の名前がかわりました\n%s -> %s", ev.NewName, ev.OldName, ev.NewName),
-								false)))
-				default:
-					log.Println("dismiss: ", ev)
+				switch eventsAPIEvent.Type {
+				case slackevents.CallbackEvent:
+					procInnerEvent(api, eventsAPIEvent.InnerEvent, policeChannelID)
 				}
 			}
 		}
-	})
+	}(api, policeChannelID)
 	fmt.Println("[INFO] slack-police")
-	fmt.Println("[INFO] Server listening")
-	http.ListenAndServe(":"+strconv.Itoa(port), nil)
+	fmt.Println("[INFO] run websocket")
+	client.Run()
+
+}
+
+func procInnerEvent(api *slack.Client, event slackevents.EventsAPIInnerEvent, policeChannelID string) {
+	log.Println("recieved: ", event.Type)
+
+	switch ev := event.Data.(type) {
+	case *slackevents.EmojiChangedEvent:
+		switch ev.Subtype {
+		case "add":
+			log.Println(
+				api.PostMessage(
+					policeChannelID,
+					slack.MsgOptionText(
+						fmt.Sprintf("絵文字警察です👮\n:%s: %s が追加されました", ev.Name, ev.Name),
+						false)))
+		case "remove":
+			log.Println(
+				api.PostMessage(
+					policeChannelID,
+					slack.MsgOptionText(
+						fmt.Sprintf("絵文字警察です👮\n%s が消えました", ev.Names[0]),
+						false)))
+		case "rename":
+			log.Println(
+				api.PostMessage(
+					policeChannelID,
+					slack.MsgOptionText(
+						fmt.Sprintf("絵文字警察です👮\n:%s: の名前がかわりました\n%s -> %s", ev.NewName, ev.OldName, ev.NewName),
+						false)))
+		default:
+			log.Println("dismiss: ", ev)
+		}
+	}
 }
